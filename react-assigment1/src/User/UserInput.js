466e126e46b059707a01e51db6df06353f207b1d
davidhartman00 <davidhartman00@gmail.com>
2019-06-06 13:23:39 -0700

    but fix

--- react-assigment1/src/User/UserInput.js
+++ react-assigment1/src/User/UserInput.js
@@ -14,7 +14,7 @@ const userInput = (props) => {
                 <input 
                 style={style}
                 onChange={props.changeUsername}
-                value={props.username} placeholder="enter a username"/>
+                value={props.value} placeholder="enter a username"/>
             </div>
         )
     
