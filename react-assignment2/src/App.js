ee66697f06ad621743e42953904612660670f7ac
davidhartman00 <davidhartman00@gmail.com>
2019-06-07 14:11:50 -0700

    updated component

--- react-assignment2/src/App.js
+++ react-assignment2/src/App.js
@@ -1,27 +1,47 @@
 import React from 'react';
 import './App.css';
 import ValidationComponent from "./ValidationComponent/ValidationComponent";
+import CharComponent from "./CharComponent/CharComponent";
 
 class App extends React.Component {
-  
-    state = {
-       length: 0
-    }
-  
-  
+  state = {
+    length: 0,
+    message: ""
+  };
 
-  textLengthHandler = (event) =>{
+  messageHandler = event => {
     this.setState({
-      length: event.target.value.length
+      length: event.target.value.length,
+      message: event.target.value
+    });
+  };
+
+  charDeleteHandler = (index) =>{
+    let message = this.state.message
+
+    message = message.split("") //turn into array
+
+    message.splice(index, 1) //remove element from array
+
+    message = message.join("") //join arry into string
+
+    this.setState({
+      message: message
     })
   }
 
-  render(){
+  render() {
     return (
       <div className="App">
-        <input onChange={this.textLengthHandler} />
+        <input type="text"
+        onChange={this.messageHandler}
+        value={this.state.message}/>
         <p>{this.state.length}</p>
-        <ValidationComponent  length={this.state.length} />
+        <ValidationComponent length={this.state.length} />
+        <CharComponent
+          message={this.state.message}
+          charDelete={this.charDeleteHandler}
+        />
       </div>
     );
   }
--- react-assignment2/src/ValidationComponent/ValidationComponent.js
+++ react-assignment2/src/ValidationComponent/ValidationComponent.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 const ValidationComponent = props => {
     let str = "Text too short";
-    if (props.length > 8 ) str = "Text long enough";
+    if (props.length > 6 ) str = "Text long enough";
     
     return (
         <div>
+            <p>
             {str}
+            </p>
         </div>
     )
 }
